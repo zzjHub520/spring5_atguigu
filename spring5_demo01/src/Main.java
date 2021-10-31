22cdc267e011d2a1e8e2dc7f54c21274b3a2160f
kooma <912290893@qq.com>
2021-10-31 15:44:33 +0800

    asdf

--- spring5_demo01/src/Main.java
+++
@@ -1,8 +0,0 @@
-import com.atguigu.spring5.User;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println( "hadsffasdffawefasa");
-        new User().add();
-    }
-}
